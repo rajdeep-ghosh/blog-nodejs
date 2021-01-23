ee2d7e55f4f1e17507e02e565a125d8a92713d13
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-01-23 06:17:31 +0530

    Refactor and capitalize post title

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui 
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-const posts = [];
-
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -74,23 +72,11 @@ app.get("/posts/:postid", (req, res) => {
       console.log(err);
     }
   });
-
-
-
-  // posts.forEach(post => {
-  //   if(_.kebabCase(post.title) === _.kebabCase(req.params.postName)) {
-  //     // console.log("Match found!");
-  //     res.render("post", {
-  //       postTitle: post.title,
-  //       postBody: post.content
-  //     });
-  //   }
-  // });
 });
 
 app.post("/compose", (req, res) => {
   const post = new Post({
-    title: req.body.postTitle,
+    title: _.capitalize(req.body.postTitle),
     content: req.body.postBody
   });
   post.save(function (err) {
